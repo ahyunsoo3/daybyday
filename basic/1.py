f7860fb69a97b72f29562f4bb9ea6c66f9f5deeb
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2021-12-26 16:27:16 +0900

    Update 1.py
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    return index.
    return indices : of elements
    
    add up to target
    : produce a particular result.
    
    Then, now I understand this through the interpreter - google dictionary.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.

--- basic/1.py
+++ basic/1.py
@@ -1,6 +1,28 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #original
+    #def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+    def twoSum(nums, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
+
+
+# 1 2 3 4 / 5
+
+# 2+3 = 5
+# 1+4 = 5
+# The soultion want exactly one solution. So, I need just one.
+
+
                 if nums[j] == target - nums[i]:
                     return [i, j]
+
+
+
+
+
+sol = Solution
+
+res = sol.twoSum(nums=[1,2,3,4], target=5)
+
+print(res)
