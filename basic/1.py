6197fa5d7293f8efb324800c415966dcb6c167cd
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2021-12-27 23:04:14 +0900

    Update 1.py
    What did I learn?
    - A case of experiencing how mathematical perspectives are used.

--- basic/1.py
+++ basic/1.py
@@ -1,35 +1,20 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
 import random
 
 class Solution:
-    #original
-    #def twoSum(self, nums: List[int], target: int) -> List[int]:
 
     def twoSum(nums, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
-
-
-# 1 2 3 4 / 5
-
-# 2+3 = 5
-# 1+4 = 5
-# The soultion want exactly one solution. So, I need just one.
-
-
-                if nums[j] == target - nums[i]:
+                if nums[j] == target - nums[i]: # it is just transition.
                     return [i, j]
 
 
-
-
-
 sol = Solution
+sp = random.sample(range(0,10), 5)
+res = sol.twoSum(nums = sp, target = 5)
 
-#res = sol.twoSum(nums=[1,2,3,4], target=5)
-
-nums = random.sample(range(0,10), 5)
-res = sol.twoSum(nums=nums, target = 5)
-
-print(nums)
+print(sp)
 print('-------------------')
 print(res)
