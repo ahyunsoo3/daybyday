a3a80c35ef81f1a30c48cc8aa13a47a003627660
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2021-12-29 21:53:21 +0900

    Update 9.py
    Revision

--- basic/9.py
+++ basic/9.py
@@ -1,10 +1,5 @@
 # Given an integer x, return true if x is palindrome integer.
-# An integer is a palindrome when it reads the same backward as forward.
-# For example, 121 is a palindrome while 123 is not.
-
 
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
+    def isPalindrome(self, x):
         return False if x < 0 else x == int(str(x)[::-1])
-
-        # Why Leetcode has only few cases? I am annoying that this solution must be the most efficient one.
