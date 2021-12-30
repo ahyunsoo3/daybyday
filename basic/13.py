68ec4a0c797d7b568357648089fd848600cbe88a
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2021-12-30 22:27:20 +0900

    Create 13.py
    https://leetcode.com/problems/roman-to-integer/

---
+++ basic/13.py
@@ -0,0 +1,21 @@
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+# Given a roman numeral, convert it to an integer.
+
+# I can be placed before V (5) and X (10) to make 4 and 9. 
+# X can be placed before L (50) and C (100) to make 40 and 90. 
+# C can be placed before D (500) and M (1000) to make 400 and 9
+
+
+class Solution:
+# @param {string} s
+# @return {integer}
+    def romanToInt(self, s):
+        roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1} #dictionary
+        z = 0
+        for i in range(0, len(s) - 1):
+            if roman[s[i]] < roman[s[i+1]]:
+                z -= roman[s[i]] #is this needed?
+            else:
+                z += roman[s[i]]
+        return z + roman[s[-1]]
