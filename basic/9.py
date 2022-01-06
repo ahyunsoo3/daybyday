17b24d2d2770646dc7127e20231bd89416fb85b9
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-01-07 04:25:49 +0900

    Must be
    there are steps to complete architecture of yours.

--- basic/9.py
+++ basic/9.py
@@ -19,3 +19,7 @@ print(x[0:])
 print(x[1:])
 
 print(x[::-1])
+
+
+# it is more important that you can understand a code than coding.
+# coding is the next step of understanding. that is, it have to be with previous steps.
