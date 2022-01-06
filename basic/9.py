f01df18d1d3cc5b8cb30b0edd27d4b89948bc271
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-01-06 22:19:18 +0900

    The basic
    the concept of basic is always important out of all.
    
    Do not neglect them due to the level of appearance.

--- basic/9.py
+++ basic/9.py
@@ -1,8 +1,21 @@
 # Given an integer x, return true if x is palindrome integer.
 
-class Solution:
-    def isPalindrome(self, x):
-        if x < 0:
-            return False
-        elif x == int(str(x)[::-1]):
-            return
+# class Solution:
+#     def isPalindrome(self, x):
+#         if x < 0:
+#             return False
+#         elif x == int(str(x)[::-1]):
+#             return
+
+
+# sl = Solution.isPalindrome()
+
+x = "abcd"
+print(x)
+print(x[:-1])
+print(x[:1])
+
+print(x[0:])
+print(x[1:])
+
+print(x[::-1])
