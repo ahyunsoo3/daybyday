31b3f6db541d0cff9b4548a5484d7a83424712db
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-01-08 14:14:14 +0900

    Visualization
    It is important than just seeing structure in the code because it helps me focus on the architecture.

--- basic/3.py
+++ basic/3.py
@@ -3,12 +3,26 @@ class Solution:
         dicts = {}
         maxlength = start = 0
         for i,value in enumerate(s):
+
+
             if value in dicts:
                 sums = dicts[value] + 1
+
+
                 if sums > start:
                     start = sums
+
+
+
             num = i - start + 1
+
+
+
             if num > maxlength:
                 maxlength = num
+
+
             dicts[value] = i
+
+
         return maxlength
