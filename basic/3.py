275578af4ecb50b6e5f73d5f19c9ec4f74460b44
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-01-09 17:22:38 +0900

    Philosophy
    this is what I have to do

--- basic/3.py
+++ basic/3.py
@@ -5,7 +5,18 @@ class Solution:
         for i,value in enumerate(s):
 
 
-            if value in dicts:
+            if value in dicts: # this process is unnormal for me to understand procesdure, so it makes me question
+                # the value have that I attempt to understand this
+                # when do people select their tasks in their way?
+                # The answer is what do you want to focus on, so people should know what they want result from this
+                # I think, I wanted to all everything, but it makes me confused by sellecting prosedure.
+                # I think that the attitude gives me a lot of things, however, narrowing down it is more important than
+                # that attitude. I understand what I aim for, but, it is needed for arranging.
+                # If you don't, you wouldn't get anything, which provides you with deeper concept that is needed for obtainng the advanced perspectives.
+
+
+
+
                 sums = dicts[value] + 1
 
 
