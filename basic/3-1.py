3ee91cd7881c23228f7290141a0fe564dd5940c9
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-01-09 17:30:37 +0900

    Another one
    this is needed for dividing solution for understand another approach in order to break down its architecture.

---
+++ basic/3-1.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        dic, res, start, = {}, 0, 0
+        for i, ch in enumerate(s):
+            # when char already in dictionary
+            if ch in dic:
+                # check length from start of string to index
+                res = max(res, i-start)
+                # update start of string index to the next index
+                start = max(start, dic[ch]+1)
+            # add/update char to/of dictionary
+            dic[ch] = i
+        # answer is either in the beginning/middle OR some mid to the end of string
+        return max(res, len(s)-start)
+
+
+# this is another solution with commenting the codes, however, I doubt that it is valuable for me to see it.
+# Because it has all explanation step by step, so, I have to find the value in terms of my phase.
