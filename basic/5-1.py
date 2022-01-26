a2b8506c4646331b80144e4207d75bda13d706bb
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-01-26 23:44:35 +0900

    Believe yourself
    your sense will lead you to answer.

--- basic/5-1.py
+++ basic/5-1.py
@@ -1,7 +1,33 @@
-import timeit
-x = []
-y = []
-for strlen in range(10000,100001,10000):
-    x.append(strlen)
-    y.append(timeit.timeit("x==y[1:]",setup="strlen = {}; x = '1'*strlen; y = 'a'+'1'*strlen".format(strlen)))
-    print(x[-1],y[-1])
+def longestPalindrome(self, s):
+    res = ""
+    for i in range(len(s)):
+        # odd case, like "aba"
+        tmp = self.helper(s, i, i)
+        if len(tmp) > len(res):
+            res = tmp
+        # even case, like "abba"
+        tmp = self.helper(s, i, i + 1)
+        if len(tmp) > len(res):
+            res = tmp
+    return res
+
+
+# get the longest palindrome, l, r are the middle indexes
+# from inner to outer
+def helper(self, s, l, r):
+    while l >= 0 and r < len(s) and s[l] == s[r]:
+        l -= 1;
+        r += 1
+    return s[l + 1:r]
+
+
+# import timeit
+# x = []
+# y = []
+# for strlen in range(10000,100001,10000):
+#     x.append(strlen)
+#     y.append(timeit.timeit("x==y[1:]",setup="strlen = {}; x = '1'*strlen; y = 'a'+'1'*strlen".format(strlen)))
+#     print(x[-1],y[-1])
+
+
+# https://leetcode.com/problems/longest-palindromic-substring/discuss/2954/Python-easy-to-understand-solution-with-comments-(from-middle-to-two-ends).
