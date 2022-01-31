b4a996993cdb2bbced84fc0b88a95dd148d2d684
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-02-01 06:54:12 +0900

    I found the material that maximize my practice

--- HackerRank/1week_kit_1.py
+++ HackerRank/1week_kit_1.py
@@ -13,11 +13,28 @@ import sys
 #
 
 def plusMinus(arr):
-    # Write your code here
+
+    pos_pos = 0
+    pos_neg = 0
+    pos_zero = 0
+    len_arr = len(arr)
+    for i in range(len(arr)):
+        if arr[i] == 0:
+            pos_zero += 1 / len_arr
+        elif arr[i] < 0:
+            pos_neg += 1 / len_arr
+        elif arr[i] > 0:
+            pos_pos += 1 / len_arr
+
+    print(pos_pos)
+    print(pos_neg)
+    print(pos_zero)
+
 
 if __name__ == '__main__':
-    n = int(input().strip())
+    # n = int(input().strip())
+    #
+    # arr = list(map(int, input().rstrip().split()))
 
-    arr = list(map(int, input().rstrip().split()))
-
+    arr = [1,5,3,-1,0,-2]
     plusMinus(arr)
