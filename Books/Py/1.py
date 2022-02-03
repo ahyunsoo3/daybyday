c7c5296b5e727efb49013b0a665144adc85a17cd
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-02-03 16:48:10 +0900

    New Samples
    - Interesting

---
+++ Books/Py/1.py
@@ -0,0 +1,14 @@
+
+# - 1 -
+# sample = [1,2,3,4,5]
+# tlist = list(map(lambda x: x + 10, sample))
+#
+# print(tlist)
+
+# - 2 -
+tlist = [n + 1 for n in range(1, 10 + 1) if n % 2 == 1]
+
+print(tlist)
+
+
+
