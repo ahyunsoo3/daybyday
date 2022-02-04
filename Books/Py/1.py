21664217dc11d82f9085747ff32910c447a3dc5b
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-02-04 11:32:43 +0900

    Follow planning
    - intuition is motivated for me, but as you know, mostly, it apple to everything spontaneously, So adjusting is important so that it is well done.

--- Books/Py/1.py
+++ Books/Py/1.py
@@ -6,9 +6,23 @@
 # print(tlist)
 
 # - 2 -
-tlist = [n + 1 for n in range(1, 10 + 1) if n % 2 == 1]
+# tlist = [n + 1 for n in range(1, 10 + 1) if n % 2 == 1]
+#
+# print(tlist)
 
-print(tlist)
+
+# - Generator -
+
+# def get_natural_number():
+#     yield 1
+#     yield 'string'
+#     yield True
+#
+# g = get_natural_number()
+# for _ in range(0, 3):
+#     print(next(g))
+
+
 
 
 
