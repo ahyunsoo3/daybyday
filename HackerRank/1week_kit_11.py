bcb769da57ef7ae89ad98bb7cc516ae86a150565
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-02-21 18:00:35 +0900

    +
    - there is what I expected

---
+++ HackerRank/1week_kit_11.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superDigit' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING n
+#  2. INTEGER k
+#
+
+def superDigit(n, k):
+    # Write your code here
+    sum = 0
+    n = str(n)
+
+    for i in n:
+        sum += int(i)
+
+    if k != 0:
+        superDigit(sum, k-1)
+    else:
+        print(sum)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    first_multiple_input = input().rstrip().split()
+
+    n = first_multiple_input[0]
+
+    k = int(first_multiple_input[1])
+
+    result = superDigit(n, k)
+
