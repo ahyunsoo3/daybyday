6009661ee77474293266f238be15908d30070589
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-02-28 10:07:01 +0900

    Create 7-1.py
    - The reference is pretty good.

---
+++ LeetCode/7-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sign = [1,-1][x < 0]
+
+        rst = sign * int(str(abs(x))[::-1])
+
+        return rst if -(2**31)-1 < rst < 2**31 else 0
+
+
+
+# Reference :
+# https://leetcode.com/problems/reverse-integer/discuss/132861/3-lines-Python-Solution
