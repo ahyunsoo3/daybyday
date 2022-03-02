6f41c49a52d1b4901257a2b3345fbdc519c0bf81
ahyunsoo2 <83423286+ahyunsoo2@users.noreply.github.com>
2022-03-02 23:46:39 +0900

    +
    take this

---
+++ LeetCode/10.py
@@ -0,0 +1,23 @@
+cache = {}
+def isMatch(self, s, p):
+    if (s, p) in self.cache:
+        return self.cache[(s, p)]
+    if not p:
+        return not s
+    if p[-1] == '*':
+        if self.isMatch(s, p[:-2]):
+            self.cache[(s, p)] = True
+            return True
+        if s and (s[-1] == p[-2] or p[-2] == '.') and self.isMatch(s[:-1], p):
+            self.cache[(s, p)] = True
+            return True
+    if s and (p[-1] == s[-1] or p[-1] == '.') and self.isMatch(s[:-1], p[:-1]):
+        self.cache[(s, p)] = True
+        return True
+    self.cache[(s, p)] = False
+    return False
+
+
+# Reference :
+# https://leetcode.com/problems/regular-expression-matching/discuss/5678/Fast-Python-solution-with-backtracking-and-caching-%2B-DP-solution
+# There are more answers provided from the creator who makes this. (DP ver)
