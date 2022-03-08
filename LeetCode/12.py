51cf4f51b0ab9a2fda6684d1b4c07f60386b9583
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-03-08 12:36:17 +0900

    +
    ref

---
+++ LeetCode/12.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        d = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V',
+             4: 'IV', 1: 'I'}
+
+        res = ""
+
+        for i in d:
+            res += (num // i) * d[i]
+            num %= i
+
+        return res
+
+
+# reference :
+# https://leetcode.com/problems/integer-to-roman/discuss/6304/Python-simple-solution.
