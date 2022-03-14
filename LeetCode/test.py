a5e95a9caa397882428aa48731facff69f85e552
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-03-14 14:32:57 +0900

    +
    lambda

---
+++ LeetCode/test.py
@@ -0,0 +1,13 @@
+# python code to demonstrate working of reduce()
+
+# importing functools for reduce()
+import functools
+
+# initializing list
+lis = [1, 3, 5, 6, 2, ]
+
+#lam = lambda x: x * 2
+print((lambda x: x + 1)(21))
+
+
+# ref : https://realpython.com/python-lambda/
