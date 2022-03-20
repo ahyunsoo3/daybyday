72cc00ee40a4190b1c5883ab85a8caaca1043556
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-03-20 23:46:39 +0900

    +
    need to study concpets of this because I don't understand what I do.

--- Parallel_Programming/practice_pool.py
+++ Parallel_Programming/practice_pool.py
@@ -1,13 +1,31 @@
-import multiprocessing as mp
+from multiprocessing import Process, Value, Array
+# from multiprocessing import Process, Pipe
+#
+# def f(conn):
+#     conn.send([42, None, 'Hello'])
+#     conn.close()
+#
+# if __name__ == '__main__':
+#     parent_conn, child_conn = Pipe()
+#     p = Process(target=f, args=(child_conn,))
+#     p.start()
+#     print(parent_conn.recv())
+#     p.join()
 
-def foo(q):
-    q.put('hello')
+def f(n, a):
+    n.value = 3.1415927
+    for i in range(len(a)):
+        a[i] = -a[i]
 
-    if __name__ == '__main__':
-        mp.set_start_method('spawn')
-        q = mp.Queue()
-        p = mp.Process(target=foo, args=(q,))
-        p.start()
-        print(q.get())
-        p.join()
+if __name__ == '__main__':
+    num = Value('d', 0.0)
+    arr = Array('i', range(10))
 
+    p = Process(target=f, args=(num,arr))
+    p.start()
+    p.join()
+
+    print(num.value)
+    print(arr[:])
+
+    
