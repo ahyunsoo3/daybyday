b2d9cb0087a14d7b52f7302b54fd4634d992e608
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-03-26 18:31:50 +0900

    +

--- Parallel_Programming/practice_pool.py
+++ Parallel_Programming/practice_pool.py
@@ -1,4 +1,15 @@
-from multiprocessing.managers import BaseManager
-manager = BaseManager(address=('', 50000),authkey=b'abc')
-server = manager.get_server()
-server.serve_forever()
+from multiprocessing import Pool
+
+def f(x):
+    return x*x
+
+if __name__ == '__main__':
+    with Pool(5) as p:
+        print(p.map(f, [1,2,3]))
+
+# offers both local and remote concurrency.
+# It runs on both Unix and Windows.
+# The above example demonstrates the common practice of defining such functions
+# Therefore, child processes can successfully import that module.
+
+# Reference : https://docs.python.org/3/library/multiprocessing.html
