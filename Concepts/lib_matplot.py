bc79fe3b42458f053d9bfe5bcc1f9be8f832c957
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-04-02 22:01:30 +0900

    +

--- Concepts/lib_matplot.py
+++ Concepts/lib_matplot.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = {'a':np.arange(50),
-        'c':np.random.randint(0,50,50),
-        'd':np.random.randn(50)}
-data['b'] = data['a'] + 10 * np.random.randn(50)
-data['d'] = np.abs(data['d']) * 20
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000)
 
-plt.scatter('a','b',c='c',s='d',data=data)
-plt.xlabel('entry a')
-plt.ylabel('entry b')
+n, bins, patches = plt.hist(x, 50, density=1, facecolor='r', alpha=0.75)
+
+plt.xlabel('Intenll')
+plt.ylabel('Proba')
+plt.title('Histogram')
+plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(False)
 plt.show()
 
 # Ref : https://www.geeksforgeeks.org/python-matplotlib-an-overview/
