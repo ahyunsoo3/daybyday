8c827bbfc1d2d5efb327dbe1cf911c47bfffa73f
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-04-04 22:24:34 +0900

    +

--- Concepts/lib_pandas.py
+++ Concepts/lib_pandas.py
@@ -7,7 +7,17 @@ dates = pd.date_range("20220101", periods=2)
 
 df = pd.DataFrame(np.random.randn(2, 3), index=dates, columns=list("ABA"))
 
-print(df)
+df2 = pd.DataFrame(
+    {
+        'A': 1.0,
+        'B': pd.Timestamp('20220101'),
+        'C': pd.Series(1, index=list(range(4)), dtype='float')
+
+    }
+)
+
+print(df2)
+print(df2.dtypes)
 
 
 
