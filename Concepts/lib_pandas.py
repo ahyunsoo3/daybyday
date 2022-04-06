62b7f4fdf80cb1e603b36d30ed94c529c67f7d1a
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-04-06 22:29:08 +0900

    +
    There are somthing to be fixed.

--- Concepts/lib_pandas.py
+++ Concepts/lib_pandas.py
@@ -1,24 +1,8 @@
 import pandas as pd
-import numpy as np
 
-s = pd.Series([1, 3, 5, np.nan, 6, 8])
+df = pd.read_csv('data.csv', encoding='latin-')
 
-dates = pd.date_range("20220101", periods=2)
-
-df = pd.DataFrame(np.random.randn(2, 3), index=dates, columns=list("ABA"))
-
-df2 = pd.DataFrame(
-    {
-        'A': 1.0,
-        'B': pd.Timestamp('20220101'),
-        'C': pd.Series(1, index=list(range(4)), dtype='float')
-
-    }
-)
-
-print(df2)
-print(df2.dtypes)
+print(df.to_string())
 
 
-
-# Ref : https://pandas.pydata.org/docs/user_guide/10min.html
+# https://www.w3schools.com/python/pandas/default.asp
