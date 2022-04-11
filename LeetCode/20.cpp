ccd381f955fef6c44965cae7c7ae531809bba73f
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-04-11 23:12:07 +0900

    +

--- LeetCode/20.cpp
+++ LeetCode/20.cpp
@@ -1,29 +1,24 @@
-def fourSum(self, nums, target):
-    def findNsum(nums, target, N, result, results):
-        if len(nums) < N or N < 2 or target < nums[0]*N or target > nums[-1]*N:
-            return
-        if N == 2:
-            l,r = 0,len(nums)-1
-            while l < r:
-                s = nums[l] + nums[r]
-                if s == target:
-                    result  s.append(result + [nums[l], nums[r]])
-                    l += 1
-                    while l < r and nums[l] == nums[l-1]:
-                        l += 1
-                elif s < target:
-                    l += 1
-                else:
-                    r -= 1
-        else: # recursively reduce N
-            for i in range(len(nums)-N+1):
-                if i == 0 or (i > 0 and nums[i-1] != nums[i]):
-                    findNsum(nums[i+1:], target-nums[i], N-1, result+[nums[i]], results)
+#include <stack>
 
-    results = []
-    findNsum(sorted(nums), target, 4, [], results)
-    return results
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> paren;
+        for (char& c : s) {
+            switch (c) {
+                case '(': 
+                case '{': 
+                case '[': paren.push(c); break;
+                case ')': if (paren.empty() || paren.top()!='(') return false; else paren.pop(); break;
+                case '}': if (paren.empty() || paren.top()!='{') return false; else paren.pop(); break;
+                case ']': if (paren.empty() || paren.top()!='[') return false; else paren.pop(); break;
+                default: ; // pass
+            }
+        }
+        return paren.empty() ;
+    }
+};
 
 
+// Ref : https://leetcode.com/problems/valid-parentheses/discuss/9252/2ms-C%2B%2B-sloution
 
-# ref : https://leetcode.com/problems/4sum/discuss/8545/Python-140ms-beats-100-and-works-for-N-sum-(Ngreater2)
