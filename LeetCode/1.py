0624a91aab2608643a1e42787db4857a46e1d19a
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-04-13 22:18:18 +0900

    +

--- LeetCode/1.py
+++ LeetCode/1.py
@@ -1,5 +1,4 @@
-# Given an array of integers nums and an integer target,
-# return indices of the two numbers such that they add up to target.
+# https://leetcode.com/problems/two-sum/
 
 import random
 
@@ -9,7 +8,7 @@ class Solution:
     def two_sum(self, nums : list, target):
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
-                if nums[j] == target - nums[i]: #math
+                if nums[j] == target - nums[i]:
                     return [i, j]
 
 
