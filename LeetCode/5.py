a0eefaf9f19f930f040966bddee51f48a2a43804
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-04-18 21:18:01 +0900

    +

--- LeetCode/5.py
+++ LeetCode/5.py
@@ -2,23 +2,27 @@ class qwe:
     def longestPalindrome(self, s):
         res = ""
         for i in range(len(s)):
-            # odd case, like "aba"
-            tmp = self.helper(s, i, i)
+
+            tmp = self.helper(s, i, i) # There are no attributes, which affect the variable of 'i'.
+
             if len(tmp) > len(res):
                 res = tmp
-            # even case, like "abba"
             tmp = self.helper(s, i, i + 1)
+
+
             if len(tmp) > len(res):
                 res = tmp
+
         return res
 
 
-    # get the longest palindrome, l, r are the middle indexes
-    # from inner to outer
-    def helper(self, s, l, r):
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            l -= 1;
+    def helper(self, s, l, r): # the most important point in this algorithm, which means understanding this helps my knowledge of this structure.
+
+        while l >= 0 and r < len(s) and s[l] == s[r]: # put breakpoint on this, and check out the procedure.
+            l -= 1
             r += 1
+
+
         return s[l + 1:r]
 
 
