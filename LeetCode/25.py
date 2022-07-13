eb996026dd98ed64e220648a6c5bbcb72676be89
ahyunsoo3 <96625665+ahyunsoo3@users.noreply.github.com>
2022-07-13 23:16:25 +0900

    +
    find keyword

---
+++ LeetCode/25.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class asd:
+    def reverseKGroup(self, head, k):
+        dummy = jump = ListNode(0)
+        dummy.next = l = r = head
+
+        while True:
+            count = 0
+            while r and count < k:  # use r to locate the range
+                r = r.next
+                count += 1
+            if count == k:  # if size k satisfied, reverse the inner linked list
+                pre, cur = r, l
+                for _ in range(k):
+                    cur.next, cur, pre = pre, cur.next, cur  # standard reversing
+                jump.next, jump, l = pre, l, r  # connect two k-groups
+            else:
+                return dummy.next
+
+asd = asd()
+#TC
+ln = ListNode(5)
+for i in range(0,4):
+    # without using next, how can I add new ListNode to tail of origin.
+
+asd.reverseKGroup()
+
+
+
+# Ref : https://leetcode.com/problems/reverse-nodes-in-k-group/discuss/11491/Succinct-iterative-Python-O(n)-time-O(1)-space
